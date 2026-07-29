d6c648892644fdbe90f3752ec075f1e96b0de0c0
1zumiii <izumiii.reflection@gmail.com>
2026-07-29 10:34:30 +0800

    Prepare 1.1.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "dev.izumi.appopsnext"
         minSdk = 35
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.1.0"
+        versionCode = 18
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
