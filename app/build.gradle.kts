53410c00aa82aaf30a53faf178d885c9475bb972
1zumiii <izumiii.reflection@gmail.com>
2026-09-18 12:23:46 +1000

    Bump version to 1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "dev.izumi.appopsnext"
         minSdk = 35
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.3.2"
+        versionCode = 31
+        versionName = "1.3.3"
         buildConfigField(
             "String",
             "SHIZUKU_API_VERSION",
